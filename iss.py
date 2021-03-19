2db2da60fc563d4d2242c2e781323de51e69e015
Ryan <ryan.pikulski@gmail.com>
2021-03-19 14:49:19 -0400

    Add files via upload

--- iss.py
+++ iss.py
@@ -4,23 +4,30 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
+import sched, time
 
+s = sched.scheduler(time.time, time.sleep)
+#Create function for getting API JSON
+def get_API():
+    iss_data = requests.get("https://api.wheretheiss.at/v1/satellites/25544")
+    response = iss_data.json()
+    s.enter(15,1 get_API)
+    return response
 
-#Get Lat and Lon from ISS API
-iss_data = requests.get("https://api.wheretheiss.at/v1/satellites/25544")
+json_response = get_API()
 
-response = iss_data.json()
+s.enter(15,1, get_API)
+s.run()
 
-print("latitue:", response["latitude"])
-print("longitude:", response["longitude"])
+df = pd.DataFrame.from_dict(json_response, orient="index")
+print("latitue:", json_response["latitude"])
+print("longitude:", json_response["longitude"])
 
-df = pd.DataFrame.from_dict(response, orient="index")
 
 
 
 #Get a Map
-
-fig = px.scatter_mapbox(df, lat=df.loc["latitude"], lon=df.loc["longitude"], color_discrete_sequence=["red"], zoom=2, height=500)
+fig = px.scatter_mapbox(df, lat=df.loc["latitude"], lon=df.loc["longitude"], color_discrete_sequence=["yellow"], zoom=2, height=500)
 
 fig.update_layout(
     mapbox_style="open-street-map",
@@ -35,7 +42,6 @@ fig.update_layout(
         }
       ])
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-# fig.show()
 
 
 #set up webpage
